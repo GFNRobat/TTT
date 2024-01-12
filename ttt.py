85287191065ac1291d77fbd06d395394c0d44742
GFNRobat <r.speiser.gfn@gmail.com>
2024-01-12 15:05:17 +0100

    added except for incorrect input

--- ttt.py
+++ ttt.py
@@ -23,23 +23,26 @@ def check():
             done = 1
 
 def pos():
-    zahl = int(input("Spieler " + spieler + " ist an der Reihe\n"))
-    if zahl > 0 and zahl < 10:
-        y = int(zahl/3)
-        x = zahl%3
-        if x != 0:
-           x -= 1
+    try:
+        zahl = int(input("Spieler " + spieler + " ist an der Reihe\n"))
+        if zahl > 0 and zahl < 10:
+            y = int(zahl/3)
+            x = zahl%3
+            if x != 0:
+                x -= 1
+            else:
+                x = 2
+                y -= 1   
         else:
-            x = 2
-            y -= 1   
-    else:
-        print("Bitte Zahl zwischen 1 und 9 eingeben.") 
-    if feld[y][x] == "---":
-        feld[y][x] = "-" + spieler + "-"
-    else:
-        print("Feld belegt...bitte anderes Feld wählen")
+            print("Bitte Zahl zwischen 1 und 9 eingeben.") 
+        if feld[y][x] == "---":
+            feld[y][x] = "-" + spieler + "-"
+        else:
+            print("Feld belegt...bitte anderes Feld wählen")
+            pos()
+    except:
+        print("Bitte Zahl zwischen 1 und 9 eingeben.")
         pos()
-
 def reihe():
     global spieler
     if spieler == "X":
